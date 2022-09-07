31a9dd352c20c781b555ad03fb7f59efb901e6a6
zyd16888 <zyd475182659@163.com>
2022-09-07 15:35:48 +0800

    adjustment code

---
+++ error_file_process.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+# 读取目录
+read_dir = "images"
+
+# 目录下的文件
+file_list = os.listdir(read_dir)
+
+# 遍历文件
+for file in file_list:
+    # 判断文件是否为正常照片
+    if file.endswith(".jpg"):
+        # 读取文件
+        with open(os.path.join(read_dir, file), "rb") as f:
+            # 读取文件内容
+            content = f.read()
+        # 判断文件内容是否为正常照片
+        if content.startswith(b"\xff\xd8\xff\xe0\x00\x10JFIF"):
+            # 如果是正常照片，不移动
+            pass
+        else:
+            # 创建坏文件夹
+            if not os.path.exists("bad"):
+                os.mkdir("bad")
+            # 如果不是正常照片，将其移动到坏目录
+            shutil.move(os.path.join(read_dir, file), os.path.join("bad", file))
+            print("move file: "+file)
+
+# 复制文件
+# num = 0
+# for file in file_list:
+#     num += 1
+#     if not os.path.exists("images_01"):
+#         os.mkdir("images_01")
+#     shutil.copy(os.path.join(read_dir, file), os.path.join("images_01", file))
+#     if num == 4000:
+#         # 结束程序
+#         exit()
--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from config import settings
 api_id  = settings.api_id
 api_hash  = settings.api_hash
 proxy = settings.proxy
+
+channel_id = settings.channel_id
+message_file = settings.message_file
+save_dir = settings.save_dir
+
+
 # Create a new Client instance
 if proxy.enable:
     app = Client("my_account", api_id=api_id, api_hash=api_hash, proxy=proxy)
@@ -17,55 +23,41 @@ else:
 date = '2022-08-23 06:15:25'
 # change date to datetime
 time6 = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-# get timestamp
-def get_timestamp():
-    return time.time()
-
-
-
 
 async def main():
     start = time.time()
     async with app:
         # Send a message, Markdown is enabled by default
         await app.send_message("me", f"Hi there! I'm using **Pyrogram**{start}")
-        # Get the message history of a chat
-        tasks = []
-        num = 0 # 计数器
-        async for message in app.get_chat_history(-1001383316433):
-            # download_image(message)
-            await save_message(message)
-            if message.photo:
-                photo = message.photo
-                num += 1
-                tasks.append(app.download_media(photo, file_name=f'./images/{photo.file_unique_id}.jpg', progress=progress, progress_args=(photo.file_unique_id, num)))
-                if num % 100 == 0: # 每100个任务开始执行一次下载
-                    await asyncio.gather(*tasks)
-                    tasks = []
-                if num % 1000 == 0:
-                    time.sleep(60) # 每1000个任务停一分钟
-        await asyncio.gather(*tasks)
-
-        # print(res)
-
+        await download_image(channel_id, "message_不绝对领域.txt", save_dir)
     end = time.time()
     print(end - start)
 
-async def save_message(message):
+async def save_message(message, file_name='message.txt'):
     # 保存消息到本地文件
-    with open('message_不绝对领域.txt', 'a', encoding="utf-8") as f:
+    with open(file_name, 'a', encoding="utf-8") as f:
         f.write(str(message))
 
 def progress(current, total, file_unique_id, num):
     print(f"{num}-{file_unique_id} {current * 100 / total:.1f}%")
 
 
+async def download_image(chat_id,message_name, save_path='./images/', limit=0, ):
+    tasks = []
+    num = 0 # 计数器
+    async for message in app.get_chat_history(chat_id, limit=limit):
+        await save_message(message, message_name)
+        if message.photo:
+            photo = message.photo
+            num += 1
+            tasks.append(app.download_media(photo, file_name=f'{save_path}', progress=progress, progress_args=(photo.file_unique_id, num)))
+            if num % 100 == 0: # 每100个任务开始执行一次下载
+                await asyncio.gather(*tasks)
+                tasks = []
+            if num % 1000 == 0:
+                time.sleep(20) # 每1000个任务停20秒
+    await asyncio.gather(*tasks)
 
-@app.on_message(filters.command("start"))
-async def download_image(message):
-    photo = message.photo
-    file_name = f'{photo.file_unique_id}.jpg'
-    await app.download_media(photo, file_name=f'./image_test/{file_name}', progress=progress)
 
 app.run(main())
 
