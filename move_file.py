6c19641891b4bbcd4eac0e3a49a6505c504d26a9
zyd16888 <zyd475182659@163.com>
2022-09-08 15:11:29 +0800

    move file

---
+++ move_file.py
@@ -0,0 +1,21 @@
+#移动文件
+import os
+import shutil
+
+# 读取目录
+read_dir = "Y:\share\图\images\images1-2999"
+out_dir = "Y:\share\图\images\images_out"
+
+# 目录下的文件
+file_list = os.listdir(read_dir)
+
+file_num = 0
+folder_num = 1
+for file in file_list:
+    file_num += 1
+    if not os.path.exists(f"{out_dir}_{folder_num}"):
+        os.mkdir(f"{out_dir}_{folder_num}")
+    shutil.move(os.path.join(read_dir, file), os.path.join(f"{out_dir}_{folder_num}", file))
+    print(f"move file: {file} to {out_dir}_{folder_num}")
+    if file_num % 1000 == 0:
+        folder_num += 1
